d3bfa956d4d08dd0b85eb05f5b47b329203e4393
waleery <walenda1997@gmail.com>
2024-02-02 20:31:56 +0100

    feat(canvas): add car objects and draw them on the canvas

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const context = canvas.getContext("2d")
+const car = new Car(100, 100, 30, 50)
+const car2 = new Car(100, 500, 30, 50)
+car.draw(context)
+car2.draw(context)
