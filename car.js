286e5fe9b37f33f9aba58db7c0942d2ae43a2b69
waleery <walenda1997@gmail.com>
2024-02-03 20:02:58 +0100

    feat(car): add car movement functionality

--- car.js
+++ car.js
@@ -7,6 +7,22 @@ class Car {
         this.controls = new Controls()
     }
 
+    update() {
+        if(this.controls.forward) {
+            this.y -= 2;
+        }
+        if(this.controls.reverse) {
+            this.y += 2;
+        }
+        if(this.controls.left) {
+            this.x -= 2;
+        }
+        if(this.controls.right) {
+            this.x += 2;
+        }
+        
+    }
+
     draw(context) {
         context.beginPath();
 
--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const canvas = document.getElementById("myCanvas")
-canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const context = canvas.getContext("2d")
-const car = new Car(100, 100, 30, 50)
-const car2 = new Car(100, 500, 30, 50)
+const car = new Car(100, 500, 30, 50)
 car.draw(context)
-car2.draw(context)
+
+animate()
+function animate() {
+    canvas.height = window.innerHeight;
+    
+    car.update()
+    car.draw(context)
+    requestAnimationFrame(animate)
+}
