4f3ec6b6b26d59b46e1796da65c51b2c9e608a97
waleery <walenda1997@gmail.com>
2024-02-05 23:58:41 +0100

    feat(car): add rotate car while turning

--- car.js
+++ car.js
@@ -4,72 +4,67 @@ class Car {
         this.y = y;
         this.width = width;
         this.height = height;
-        this.controls = new Controls()
+        this.controls = new Controls();
 
         this.speed = 0;
         this.acceleration = 0.2;
         this.maxSpeed = 3;
-        this.friction = 0.05
+        this.friction = 0.05;
+
+        this.angle = 0;
     }
 
     update() {
-        if(this.controls.forward) {
+        if (this.controls.forward) {
             this.speed += this.acceleration;
         }
-        if(this.controls.reverse) {
+        if (this.controls.reverse) {
             this.speed -= this.acceleration;
         }
 
-        if(this.speed > this.maxSpeed) {
+        if (this.speed > this.maxSpeed) {
             this.speed = this.maxSpeed;
         }
-        if(this.speed < -this.maxSpeed/2) {
-            this.speed = -this.maxSpeed/2;
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
         }
 
-        if(this.speed > 0) {
+        if (this.speed > 0) {
             this.speed -= this.friction;
         }
-        if(this.speed < 0) {
+        if (this.speed < 0) {
             this.speed += this.friction;
         }
-        if(Math.abs(this.speed) < this.friction) {
+        if (Math.abs(this.speed) < this.friction) {
             this.speed = 0;
         }
 
         this.y -= this.speed;
 
-        if(this.controls.left) {
-            this.x -= 2;
+        if (this.controls.left) {
+            this.angle += 0.03;
         }
-        if(this.controls.right) {
-            this.x += 2;
+        if (this.controls.right) {
+            this.angle -= 0.03;
         }
-        
     }
 
     draw(context) {
+        context.save();
+        context.translate(this.x, this.y);
+        context.rotate(-this.angle);
+
         context.beginPath();
 
         // The rectangle is centered at (x, y) and has the size (width, height)
         context.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            -this.width / 2,
+            -this.height / 2,
             this.width,
             this.height
         );
         context.fill();
 
-
-        context.beginPath();
-
-        // Draws a circle centered at (x, y) with radius 3
-        context.arc(this.x, this.y, 3, 0, 2 * Math.PI);
-        
-        context.fillStyle = "green";
-        context.fill();
-
-        //reset the fill color to black
-        context.fillStyle = "black";
+        context.restore();
     }
 }
