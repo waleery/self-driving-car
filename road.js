b7ab6d88dfa885cd32b5ef37cf78e6d0c74b7a6f
waleery <walenda1997@gmail.com>
2024-02-08 16:28:34 +0100

    feat(road): created road class with constructor

---
+++ road.js
@@ -0,0 +1,15 @@
+class Road {
+    constructor(x, width, laneCount = 3) {
+        this.x = x;
+        this.width = width;
+        this.laneCount = laneCount;
+
+        this.left = x - width / 2;
+        this.right = x + width / 2;
+
+        //if we draw with js infinity, werid thing happens
+        const infinity = 1000000
+        this.top = -infinity
+        this.bottom = infinity
+    }
+}
