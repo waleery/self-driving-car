4bbdcb76a9b7d19dc395f7ae8bcb86448223aff8
waleery <walenda1997@gmail.com>
2024-02-08 19:04:31 +0100

    feat(road): draw borders separately

--- road.js
+++ road.js
@@ -14,13 +14,25 @@ class Road {
         const infinity = 1000000;
         this.top = -infinity;
         this.bottom = infinity;
+
+        const topLeft = { x: this.left, y: this.top };
+        const topRight = { x: this.right, y: this.top };
+        const bottomLeft = { x: this.left, y: this.bottom };
+        const bottomRight = { x: this.right, y: this.bottom };
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight],
+        ];
     }
 
     getLaneCenter(laneIndex) {
         const laneWidth = this.width / this.laneCount;
 
-        //    start road + (               which lane                      ) + ( to center on lane)
-        return this.left + Math.min(laneIndex, this.laneCount-1) * laneWidth + laneWidth/2;
+        return (
+            this.left +                                           // start road
+            Math.min(laneIndex, this.laneCount - 1) * laneWidth + // which lane
+            laneWidth / 2                                         // to center on lane
+        );
     }
 
     draw(context) {
@@ -28,23 +40,28 @@ class Road {
         context.strokeStyle = "white";
 
         //drawing lines on the road
-        for (let i = 0; i <= this.laneCount; i++) {
+        //starting at 1 and "<" not "<=" because borders are drawing separately
+        for (let i = 1; i < this.laneCount; i++) {
             const x = lineralInterpolation(
                 this.left,
                 this.right,
                 i / this.laneCount
             );
 
-            if (i > 0 && i < this.laneCount) {
-                context.setLineDash([30, 25]);
-            } else {
-                context.setLineDash([]);
-            }
-
+            context.setLineDash([30, 25]);
             context.beginPath();
             context.moveTo(x, this.top);
             context.lineTo(x, this.bottom);
             context.stroke();
         }
+
+        //drawing borders
+        context.setLineDash([])
+        this.borders.forEach(border=>{
+            context.beginPath();
+            context.moveTo(border[0].x, border[0].y)
+            context.lineTo(border[1].x, border[1].y)
+            context.stroke()
+        })
     }
 }
