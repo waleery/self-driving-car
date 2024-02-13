bc3bc31dc35b97eb2a9a269ff2485fdb0f709d75
waleery <walenda1997@gmail.com>
2024-02-13 23:16:14 +0100

    feat(car): add maxSpeed parameter to Car constructor

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const context = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 500, 30, 50, "KEYS");
 const traffic = [
-    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY")
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY",2)
 ]
 
 animate();
