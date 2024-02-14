7d0211e15860e29bb2dfb4047fc8cac78052272e
waleery <walenda1997@gmail.com>
2024-02-14 14:25:19 +0100

    feat(sensor): change sensor properties

--- sensor.js
+++ sensor.js
@@ -1,9 +1,9 @@
 class Sensor {
     constructor(car) {
         this.car = car;
-        this.rayCount = 3;
-        this.rayLength = 100;
-        this.raySpread = Math.PI / 4; //45
+        this.rayCount = 5;
+        this.rayLength = 150;
+        this.raySpread = Math.PI / 2; //45
 
         this.rays = [];
         this.readings=[]
