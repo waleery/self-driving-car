1ff6ca634713814774244991e04c37aef7292bab
waleery <walenda1997@gmail.com>
2024-02-14 15:03:57 +0100

    feat(network): add static method 'randomize'

--- network.js
+++ network.js
@@ -1,15 +1,28 @@
 class Level {
-    constructor(inputCount, outputCount){
-        this.inputs = new Array(inputCount)
-        this.outputs = new Array(outputCount)
-        //each output neuron has a bias (value) which it will fire action 
-        this.biases = new Array(outputCount)
+    constructor(inputCount, outputCount) {
+        this.inputs = new Array(inputCount);
+        this.outputs = new Array(outputCount);
+        //each output neuron has a bias (value) which it will fire action
+        this.biases = new Array(outputCount);
 
-        this.weights = []
-        for(let i = 0; i<inputCount; i++){
+        this.weights = [];
+        for (let i = 0; i < inputCount; i++) {
             //weights for each connection from input to output
-            this.weights[i] = new Array(outputCount)
+            this.weights[i] = new Array(outputCount);
         }
 
+    }
+
+    static randomize(level) {
+        for (let i = 0; i < level.inputs.length; i++) {
+            for (let j = 0; j < level.outputs.length; j++) {
+                //random number between -1 and 1
+                //negative values means to dont do something
+                level.weights[i][j] = Math.random() * 2 - 1;
+            }
+        }
+        for (let i = 0; i < level.biases.length; i++) {
+            level.biases[i] = Math.random() * 2 - 1;
+        }
     }
 }
