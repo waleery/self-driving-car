c4051a087f0ce7b5e9504acf7cb7ec5b4572c15a
waleery <walenda1997@gmail.com>
2024-02-19 00:20:35 +0100

    feat(visualizer): drawing first layer of dots which represents rays by new drawLevel method

--- visualizer.js
+++ visualizer.js
@@ -6,6 +6,31 @@ class Visualizer {
         const width = context.canvas.width - margin * 2;
         const height = context.canvas.height - margin * 2;
 
+        Visualizer.drawLevel(
+            context,
+            network.levels[0],
+            left,
+            top,
+            width,
+            height
+        );
+    }
 
+    static drawLevel(context, level, left, top, width, height) {
+        const right = left + width;
+        const bottom = top + height;
+
+        const nodeRadius = 20;
+        for (let i = 0; i < level.inputs.length; i++) {
+            const x = lineralInterpolation(
+                left,
+                right,
+                (level.inputs.length == 1 ? 0.5 : i / (level.inputs.length - 1))
+            );
+            context.beginPath()
+            context.arc(x, bottom, nodeRadius, 0, Math.PI*2)
+            context.fillStyle = "white"
+            context.fill()
+        }
     }
 }
