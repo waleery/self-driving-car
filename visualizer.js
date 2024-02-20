e9250531ed155e98b19d28b0e59989ad3b58e57e
waleery <walenda1997@gmail.com>
2024-02-20 12:08:14 +0100

    feat(visualizer): draw arrow in the last level of neural network

--- visualizer.js
+++ visualizer.js
@@ -6,21 +6,32 @@ class Visualizer {
         const width = context.canvas.width - margin * 2;
         const height = context.canvas.height - margin * 2;
 
-        const levelHeight = height/network.levels.length
+        const levelHeight = height / network.levels.length;
 
-        for(let i = network.levels.length - 1; i >=0 ; i--){
-            const levelTop = top + 
+        for (let i = network.levels.length - 1; i >= 0; i--) {
+            const levelTop =
+                top +
                 lineralInterpolation(
                     height - levelHeight,
                     0,
-                    network.levels.length == 1 ? 0.5 : i/(network.levels.length-1)
-                )
-            
-                Visualizer.drawLevel(context, network.levels[i], left, levelTop, width, levelHeight)
-        }   
+                    network.levels.length == 1
+                        ? 0.5
+                        : i / (network.levels.length - 1)
+                );
+
+            Visualizer.drawLevel(
+                context,
+                network.levels[i],
+                left,
+                levelTop,
+                width,
+                levelHeight,
+                i == network.levels.length-1 ? ['↑', '←', '→', '↓'] : []
+            );
+        }
     }
 
-    static drawLevel(context, level, left, top, width, height) {
+    static drawLevel(context, level, left, top, width, height, outputLabels) {
         const right = left + width;
         const bottom = top + height;
 
@@ -43,7 +54,7 @@ class Visualizer {
                 );
                 context.lineWidth = 2;
 
-                context.strokeStyle = getRGBA(weights[i][j])
+                context.strokeStyle = getRGBA(weights[i][j]);
                 context.stroke();
             }
         }
@@ -52,7 +63,7 @@ class Visualizer {
         for (let i = 0; i < inputs.length; i++) {
             const x = Visualizer.#getNodeX(inputs, i, left, right);
             context.beginPath();
-            context.arc(x, bottom, nodeRadius*0.7, 0, Math.PI * 2);
+            context.arc(x, bottom, nodeRadius * 0.7, 0, Math.PI * 2);
             context.fillStyle = getRGBA(inputs[i]);
             context.fill();
         }
@@ -67,18 +78,31 @@ class Visualizer {
             context.fill();
 
             context.beginPath();
-            context.arc(x, top, nodeRadius*0.7, 0, Math.PI * 2);
+            context.arc(x, top, nodeRadius * 0.7, 0, Math.PI * 2);
             context.fillStyle = getRGBA(outputs[i]);
             context.fill();
 
-            context.beginPath()
-            context.lineWidth = 2
-            context.arc(x, top, nodeRadius,0, Math.PI*2)
-            context.strokeStyle = getRGBA(biases[i])
-            context.setLineDash([3,3])
-            context.stroke()
-            context.setLineDash([])
+            context.beginPath();
+            context.lineWidth = 2;
+            context.arc(x, top, nodeRadius, 0, Math.PI * 2);
+            context.strokeStyle = getRGBA(biases[i]);
+            context.setLineDash([3, 3]);
+            context.stroke();
+            context.setLineDash([]);
 
+            //draw arrows in the last level
+            if(outputLabels[i]){
+                context.beginPath()
+                context.textAlign="center",
+                context.textBaseline = "middle",
+                context.fillStyle = "black",
+                context.strokeStyle = "white"
+                context.font = (nodeRadius *1.3) + "px Arial"
+                context.fillText(outputLabels[i], x, top)
+                context.lineWidth = 0.5
+                context.strokeText(outputLabels[i], x, top)
+
+            }
         }
     }
     //helper method
