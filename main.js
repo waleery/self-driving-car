2a8bb0636f0399c9c6bbe26466056b8b54dd1ebe
waleery <walenda1997@gmail.com>
2024-02-21 20:07:18 +0100

    feat: generate "N" cars on the road

--- main.js
+++ main.js
@@ -8,41 +8,67 @@ const carContext = carCanvas.getContext("2d");
 const networkContext = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 500, 30, 50, "KEYS");
+
+//const cars = [new Car(road.getLaneCenter(1), 500, 30, 50, "KEYS")];
+
+const N = 100
+const cars = generateCars(N)
+
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY",2)
 ]
 
 animate();
+
+function generateCars(N){
+    const cars = []
+    for(let i = 0; i<= N; i++){
+        cars.push(new Car(road.getLaneCenter(1), 500, 30, 50, "AI"))
+    }
+    return cars
+}
+
 function animate(time) {
     //if we change carCanvas height on each frame, we dont need to clear carCanvas
     carCanvas.height = window.innerHeight;
     networkCanvas.height = window.innerHeight;
     //carContext.clearRect(0,0, carCanvas.width, window.innerHeight)
 
+    //update traffic cars positions
     for(let i = 0; i<traffic.length; i++){
         traffic[i].update(road.borders,[])
     }
 
-    car.update(road.borders, traffic);
+    //update AI cars positions
+    for(i = 0; i< cars.length; i++){
+        cars[i].update(road.borders, traffic)
+    }
 
     //save carContext, because on each frame translate would be added
     carContext.save();
 
     //to make illusion that camera is above the car
-    carContext.translate(0, -car.y + carCanvas.height *0.75);
+    carContext.translate(0, -cars[0].y + carCanvas.height *0.75);
     
+    //draw road
     road.draw(carContext);
+
+    //draw traffic cars
     for(let i = 0;i<traffic.length;i++){
         traffic[i].draw(carContext, "red")
     }
-    car.draw(carContext, "blue");
+
+    //draw AI cars
+    for(let i = 0;i<cars.length;i++){
+        cars[i].draw(carContext, "blue")
+    }
+    
     
     //restore carContext to state before translate
     carContext.restore();
 
     //animate line between nodes
     networkContext.lineDashOffset= -time/50 // '-' to change direction of movement
-    Visualizer.drawNetwork(networkContext, car.brain)
+    Visualizer.drawNetwork(networkContext, cars[0].brain)
     requestAnimationFrame(animate);
 }
