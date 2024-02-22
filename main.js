853241089c2bd277ea756f977ab5411c5a5c3ac8
waleery <walenda1997@gmail.com>
2024-02-23 00:06:36 +0100

    feat: add buttons to save and discard brain

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ if(localStorage.getItem("bestBrain")){
 }
 
 const traffic = [
-    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY",2)
+    new Car(road.getLaneCenter(1), 400, 30, 50, "DUMMY",2)
 ]
 
 animate();
