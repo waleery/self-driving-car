96f8c5af7aef9233731ae7020879b0e91329fdb0
waleery <walenda1997@gmail.com>
2024-02-22 23:57:28 +0100

    feat: auto fetching bestBrain from local storage

--- main.js
+++ main.js
@@ -14,6 +14,9 @@ const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const N = 100
 const cars = generateCars(N)
 let bestCar=cars[0]
+if(localStorage.getItem("bestBrain")){
+    bestCar.brain = JSON.parse(localStorage.getItem("bestBrain"))
+}
 
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY",2)
