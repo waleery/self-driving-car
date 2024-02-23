3357e890d9433e87578ed3b738e04e6a2c4023fa
waleery <walenda1997@gmail.com>
2024-02-24 00:45:31 +0100

    feat: add 4 more cars in the traffic

--- main.js
+++ main.js
@@ -31,6 +31,10 @@ const traffic = [
     new Car(road.getLaneCenter(1), 300, 30, 50, "DUMMY",2),
     new Car(road.getLaneCenter(0), 150, 30, 50, "DUMMY",2),
     new Car(road.getLaneCenter(2), 150, 30, 50, "DUMMY",2),
+    new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY",2),
+    new Car(road.getLaneCenter(1), -300, 30, 50, "DUMMY",2),
+    new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY",2),
+    new Car(road.getLaneCenter(2), -500, 30, 50, "DUMMY",2),
 ]
 
 animate();
